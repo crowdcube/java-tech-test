656cc4e2468d4f92a867e904101571bda5e75b34
Paul Grycuk <paul.grycuk@crowdcube.com>
2021-09-08 10:18:54 +0100

    Updates following initial code review

--- src/test/java/com/ccmdb/movies/controller/MovieControllerTest.java
+++ src/test/java/com/ccmdb/movies/controller/MovieControllerTest.java
@@ -1,14 +1,11 @@
 package com.ccmdb.movies.controller;
 
-import com.ccmdb.movies.tc.AbstractContainerBaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(initializers = AbstractContainerBaseTest.MockServerInitializer.class)
-class MovieControllerTest extends AbstractContainerBaseTest {
+class MovieControllerTest {
 
     @LocalServerPort
     int port;
